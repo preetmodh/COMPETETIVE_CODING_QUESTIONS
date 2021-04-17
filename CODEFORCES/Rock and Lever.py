04b5c7d8dbfb827b27005d1828ed8aacdb286df7
PREET MODH <preetmodh@gmail.com>
2021-04-17 14:58:42 +0530

    Add files via upload

---
+++ CODEFORCES/Rock and Lever.py
@@ -0,0 +1,326 @@
+"""
+
+Author : Preet Modh 
+
+"""
+import math
+import sys,os
+from difflib import SequenceMatcher
+from collections import Counter
+from bisect import bisect_left as bl,bisect_right as br
+from io import BytesIO, IOBase
+from itertools import accumulate
+
+
+""" 
+
+**All function calls:**
+
+LCSubstr(a,b):              for Longest Common Substring
+gcd(x, y):                  greatest common divisor of x and y
+prime_factors(n):           Number of prime factor of n
+distinct_factors(n):        list of distinct factors of n
+all_factors(n):             list of all factors of n
+is_prime(n):                checks if n is prime
+prime_list(n):              all primes less than number of
+modinv(a, m):               modular inverse of a w.r.t. to m, works when a and m are coprime
+make_nCr_mod()(n,r)         value of ncr%mod
+binarySearch(arr, l, r, x): binary search 
+PrefixSum(arr):             prefix sum array
+memodict(f):                Memoization decorator for a function taking a single argument
+memoize(f):                 Memoization decorator for a function taking one or more arguments
+
+"""
+
+
+# Fast IO Region
+BUFSIZE = 8192
+class FastIO(IOBase):
+    newlines = 0
+    def __init__(self, file):
+        self._fd = file.fileno()
+        self.buffer = BytesIO()
+        self.writable = "x" in file.mode or "r" not in file.mode
+        self.write = self.buffer.write if self.writable else None
+    def read(self):
+        while True:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            if not b:
+                break
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines = 0
+        return self.buffer.read()
+    def readline(self):
+        while self.newlines == 0:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            self.newlines = b.count(b"\n") + (not b)
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines -= 1
+        return self.buffer.readline()
+    def flush(self):
+        if self.writable:
+            os.write(self._fd, self.buffer.getvalue())
+            self.buffer.truncate(0), self.buffer.seek(0)
+class IOWrapper(IOBase):
+    def __init__(self, file):
+        self.buffer = FastIO(file)
+        self.flush = self.buffer.flush
+        self.writable = self.buffer.writable
+        self.write = lambda s: self.buffer.write(s.encode("ascii"))
+        self.read = lambda: self.buffer.read().decode("ascii")
+        self.readline = lambda: self.buffer.readline().decode("ascii")
+sys.stdin, sys.stdout = IOWrapper(sys.stdin), IOWrapper(sys.stdout)
+input = lambda: sys.stdin.readline().rstrip("\r\n")
+
+
+def memodict(f):
+    """ Memoization decorator for a function taking a single argument. """
+    class memodict(dict):
+        def __missing__(self, key):
+            ret = self[key] = f(key)
+            return ret
+
+    return memodict().__getitem__
+
+
+def memoize(f):
+    """ Memoization decorator for a function taking one or more arguments. """
+    class memodict(dict):
+        def __getitem__(self, *key):
+            return dict.__getitem__(self, key)
+
+        def __missing__(self, key):
+            ret = self[key] = f(*key)
+            return ret
+
+    return memodict().__getitem__
+
+
+LCSubstr = lambda a, b:SequenceMatcher(None, a, b).find_longest_match(0, len(a), 0, len(b))
+""" THIS IS FOR  for Longest Common Substring """
+
+
+def gcd(x, y):
+    """greatest common divisor of x and y"""
+    while y:
+        x, y = y, x % y
+    return x
+
+
+def pollard_rho(n):
+    """returns a random factor of n"""
+    if n & 1 == 0:
+        return 2
+    if n % 3 == 0:
+        return 3
+
+    s = ((n - 1) & (1 - n)).bit_length() - 1
+    d = n >> s
+    for a in [2, 325, 9375, 28178, 450775, 9780504, 1795265022]:
+        p = pow(a, d, n)
+        if p == 1 or p == n - 1 or a % n == 0:
+            continue
+        for _ in range(s):
+            prev = p
+            p = (p * p) % n
+            if p == 1:
+                return gcd(prev - 1, n)
+            if p == n - 1:
+                break
+        else:
+            for i in range(2, n):
+                x, y = i, (i * i + 1) % n
+                f = gcd(abs(x - y), n)
+                while f == 1:
+                    x, y = (x * x + 1) % n, (y * y + 1) % n
+                    y = (y * y + 1) % n
+                    f = gcd(abs(x - y), n)
+                if f != n:
+                    return f
+    return n
+
+@memodict
+def prime_factors(n):
+    """returns a Counter of the prime factorization of n"""
+    if n <= 1:
+        return Counter()
+    f = pollard_rho(n)
+    return Counter([n]) if f == n else prime_factors(f) + prime_factors(n // f)
+
+
+def distinct_factors(n):
+    """returns a list of all distinct factors of n"""
+    factors = [1]
+    for p, exp in prime_factors(n).items():
+        factors += [p**i * factor for factor in factors for i in range(1, exp + 1)]
+    return factors
+
+
+def all_factors(n):
+    """returns a sorted list of all distinct factors of n"""
+    small, large = [], []
+    for i in range(1, int(n**0.5) + 1, 2 if n & 1 else 1):
+        if not n % i:
+            small.append(i)
+            large.append(n // i)
+    if small[-1] == large[-1]:
+        large.pop()
+    large.reverse()
+    small.extend(large)
+    return small
+
+def is_prime(n):
+    """returns True if n is prime else False"""
+    if n < 5 or n & 1 == 0 or n % 3 == 0:
+        return 2 <= n <= 3
+    s = ((n - 1) & (1 - n)).bit_length() - 1
+    d = n >> s
+    for a in [2, 325, 9375, 28178, 450775, 9780504, 1795265022]:
+        p = pow(a, d, n)
+        if p == 1 or p == n - 1 or a % n == 0:
+            continue
+        for _ in range(s):
+            p = (p * p) % n
+            if p == n - 1:
+                break
+        else:
+            return False
+    return True
+
+def prime_sieve(n):
+    """returns a sieve of primes >= 5 and < n"""
+    flag = n % 6 == 2
+    sieve = bytearray((n // 3 + flag >> 3) + 1)
+    for i in range(1, int(n**0.5) // 3 + 1):
+        if not (sieve[i >> 3] >> (i & 7)) & 1:
+            k = (3 * i + 1) | 1
+            for j in range(k * k // 3, n // 3 + flag, 2 * k):
+                sieve[j >> 3] |= 1 << (j & 7)
+            for j in range(k * (k - 2 * (i & 1) + 4) // 3, n // 3 + flag, 2 * k):
+                sieve[j >> 3] |= 1 << (j & 7)
+    return sieve
+
+
+def prime_list(n):
+    """returns a list of primes <= n"""
+    res = []
+    if n > 1:
+        res.append(2)
+    if n > 2:
+        res.append(3)
+    if n > 4:
+        sieve = prime_sieve(n + 1)
+        res.extend(3 * i + 1 | 1 for i in range(1, (n + 1) // 3 + (n % 6 == 1)) if not (sieve[i >> 3] >> (i & 7)) & 1)
+    return res
+    
+
+
+
+def extended_gcd(a, b):
+    """returns gcd(a, b), s, r s.t. a * s + b * r == gcd(a, b)"""
+    s, old_s = 0, 1
+    r, old_r = b, a
+    while r:
+        q = old_r // r
+        old_r, r = r, old_r - q * r
+        old_s, s = s, old_s - q * s
+    return old_r, old_s, (old_r - old_s * a) // b if b else 0
+
+
+def modinv(a, m):
+    """returns the modular inverse of a w.r.t. to m, works when a and m are coprime"""
+    g, x, _ = extended_gcd(a % m, m)
+    return x % m if g == 1 else None
+
+
+def make_nCr_mod(max_n=2 * 10**5, mod=10**9 + 7):
+    max_n = min(max_n, mod - 1)
+
+    fact, inv_fact = [0] * (max_n + 1), [0] * (max_n + 1)
+    fact[0] = 1
+    for i in range(max_n):
+        fact[i + 1] = fact[i] * (i + 1) % mod
+
+    inv_fact[-1] = pow(fact[-1], mod - 2, mod)
+    for i in reversed(range(max_n)):
+        inv_fact[i] = inv_fact[i + 1] * (i + 1) % mod
+
+    def nCr_mod(n, r):
+        res = 1
+        while n or r:
+            a, b = n % mod, r % mod
+            if a < b:
+                return 0
+            res = res * fact[a] % mod * inv_fact[b] % mod * inv_fact[a - b] % mod
+            n //= mod
+            r //= mod
+        return res
+
+    return nCr_mod
+    """ 
+    To use:
+    x=make_nCr_mod()
+    print(x(n,r))
+    """
+
+def binarySearch(arr, l, r, x):
+  
+    while l <= r:
+        mid = l + (r - l) // 2
+        if arr[mid] == x:
+            return mid
+
+        elif arr[mid] < x:
+            l = mid + 1
+  
+            r = mid - 1
+      
+    return -1
+
+
+  
+def PrefixSum(arr):
+      return list(accumulate(arr))
+      
+      
+class inputs:
+    #To handle different inputs
+    def single(self):
+        return int(input())
+    def mul(self):
+        return map(int,input().split())
+    def list(self):
+        return  list(map(int,input().split()))
+
+inp=inputs()
+########################################################
+
+
+
+
+
+def main():
+    for t in range(int(input())):
+        n=inp.single()
+        a=inp.list()
+        l=[]
+        for i in a:
+            l.append(int(math.log2(i)))
+        l=sorted(l)
+        ans=0
+        cnt=1
+        for i in range(1,n):
+            if l[i]==l[i-1]:
+                cnt+=1
+            else:
+                ans+=cnt*(cnt-1)//2
+                cnt=1
+        ans+=cnt*(cnt-1)//2
+        print(ans)
+
+
+
+if __name__ == "__main__":
+    main()
